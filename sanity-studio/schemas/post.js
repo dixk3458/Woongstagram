0d0555c139621796a75e6bfa677d9f58483a4c52
Jaewoong <dixk3458@naver.com>
2024-01-04 16:55:27 +0900

    Post 스키마 셋업

---
+++ sanity-studio/schemas/post.js
@@ -0,0 +1,62 @@
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
+    },
+    {
+      title: 'Photo',
+      name: 'photo',
+      type: 'image',
+    },
+    {
+      title: 'Text',
+      name: 'text',
+      type: 'string',
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          // 새로운 Comment 스키마 설정
+          // 여기에서만 사용하기에 따로 file로 만들지는 않음
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}],
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
