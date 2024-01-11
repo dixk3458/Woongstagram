8fc979cff45f9917199c91441bd8bb123c962d25
Jaewoong <dixk3458@naver.com>
2024-01-11 16:01:30 +0900

    UserProfile 스타일링

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -15,21 +15,23 @@ export default function UserProfile({ user }: Props) {
     { title: 'posts', data: posts },
   ];
   return (
-    <section>
-      <Avatar image={userimage} highlight />
-      <div>
-        <h1>{userid}</h1>
-        <FollowButton user={user} />
+    <section className="w-full flex flex-col items-center justify-center py-12 border-b border-gray-400 md:flex-row ">
+      <Avatar image={userimage} highlight  size="xlarge"/>
+      <div className="md:ml-10 basis-1/3">
+        <div className="flex flex-col items-center  md:flex-row">
+          <h1 className="text-2xl md:mr-8 my-2 md:mb-0">{userid}</h1>
+          <FollowButton user={user} />
+        </div>
+        <ul className ="my-4 flex gap-4">
+          {info.map(({ title, data }, index) => (
+            <li key={index}>
+              <span className="font-bold mr-1">{data}</span>
+              {title}
+            </li>
+          ))}
+        </ul>
+        <p className="text-xl font-bold text-center md:text-start">{username}</p>
       </div>
-      <ul>
-        {info.map(({ title, data }, index) => (
-          <li key={index}>
-            <span>{data}</span>
-            {title}
-          </li>
-        ))}
-      </ul>
-      {username}
     </section>
   );
 }
