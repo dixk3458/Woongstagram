57b3385bd4c6bbb1f09fac914627cf4e97d4075c
Jaewoong <dixk3458@naver.com>
2024-05-27 17:38:32 +0900

    avatar styling

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,8 +4,13 @@ type Props = {
 
 export default function Avatar({ image }: Props) {
   return (
-    <div>
-      <img src={image ?? undefined} alt="user profile" />
+    <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-indigo-300 via-purple-300 to-pink-300 p-[0.1rem]">
+      <img
+        className="rounded-full "
+        src={image ?? undefined}
+        alt="user profile"
+        referrerPolicy="no-referrer"
+      />
     </div>
   );
 }
