c8f28c77cb6a9f0ce52bad7e581b216c7eb4a60b
Jaewoong <dixk3458@naver.com>
2024-05-30 21:39:56 +0900

    followingBar - GROQ

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,5 +1,15 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getUserByUserName } from '@/service/user';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json('test');
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
+  return getUserByUserName(user.userName).then(data => NextResponse.json(data));
 }
--- src/service/user.ts
+++ src/service/user.ts
@@ -8,7 +8,7 @@ type OAuthUser = {
   image?: string | null;
 };
 
-export function addUser({ id, name, userName, email, image }: OAuthUser) {
+export async function addUser({ id, name, userName, email, image }: OAuthUser) {
   return client.createIfNotExists({
     _id: id,
     _type: 'user',
@@ -20,4 +20,16 @@ export function addUser({ id, name, userName, email, image }: OAuthUser) {
     followers: [],
     bookmarks: [],
   });
+}
+
+export async function getUserByUserName(userName: string) {
+  return client.fetch(
+    `*[_type == 'user' && userName == "${userName}"][0]{
+      ...,
+      "id":_id,
+      "following":following[]->{userName,image},
+      "followers":followers[]->{userName,image},
+      "bookmarks":bookmarks[]->_id
+    }`
+  );
 }
