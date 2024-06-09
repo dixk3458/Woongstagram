2de2eb00eead684d8194c216dfbc3ec2de26865a
Jaewoong <dixk3458@naver.com>
2024-06-09 21:38:05 +0900

    search - component

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,28 +1,41 @@
-import { useState } from 'react';
+import { ProfileUser } from '@/model/user';
+import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
+import ProgressSpinner from './ui/ProgressSpinner';
 
 export default function UserSearch() {
   const [text, setText] = useState('');
   const {
-    data,
+    data: users,
     isLoading: loading,
     error,
-  } = useSWR(`/api/search?keyword=${text}`);
-  console.log(data);
+  } = useSWR<ProfileUser[]>(`/api/search?keyword=${text}`);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <input type="text" value={text} onChange={e => setText(e.target.value)} />
-      {/* {loading && <p>로딩중...</p>}
-      {error && <p>에러...</p>}
-      {!loading && !error && data.length < 1 ? (
-        <p>유저 없음</p>
-      ) : (
-        <ul>
-          {data.map((d: any) => (
-            <li>{d.name}</li>
-          ))}
-        </ul>
-      )} */}
+      <form onSubmit={e => handleSubmit(e)}>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+      </form>
+      {error && <p>에러가 발생했습니다.</p>}
+      {loading && <ProgressSpinner />}
+      {!error && !loading && users && users.length < 1 && (
+        <p>사용자가 없습니다.</p>
+      )}
+      <ul>
+        {users &&
+          users.length > 0 &&
+          users.map(user => <li key={user.userName}>{user.name}</li>)}
+      </ul>
     </>
   );
 }
